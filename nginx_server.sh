17a621ea8e2817367500b45ce3c97f52d01d0ced
seven6306 <31332405+seven6306@users.noreply.github.com>
2018-01-29 13:12:09 +0800

    add bakeup function
    add bake up /etc/nginx/sites-enabled/default action

--- nginx_server.sh
+++ nginx_server.sh
@@ -1,6 +1,3 @@
-#!/bin/bash
-# Script for ubuntu 14.04 LTS
-RED='\033[0;31m'
 GREEN='\033[0;32m'
 PURPLE='\033[1;35m'
 NC='\033[0m'
@@ -26,8 +23,9 @@ printf "========================================================================
 openssl req -x509 -nodes -sha256 -days 365 -newkey rsa:2048 -keyout /etc/nginx/ssl/nginx.key -out /etc/nginx/ssl/nginx.crt -subj "/C=TW/ST=TAIPEI/L=Stockholm /O=A/OU=B/CN=C/emailAddress=xxx@xxx.com"
 openssl dhparam -out /etc/nginx/ssl/dhparam.pem 4096
 printf "=============================================================================\n\n"
-sed -i.bak '/# Make site accessible from/ a \ \ \ \ \ \ \ \ ssl_ciphers "EECDH+AESGCM:EDH+AESGCM:AES256+EECDH:AES256+EDH";' /etc/nginx/sites-enabled/default
-for each_line in 'ssl_protocols TLSv1 TLSv1.1 TLSv1.2;' 'ssl_certificate_key /etc/nginx/ssl/nginx.key;' 'ssl_certificate /etc/nginx/ssl/nginx.crt;' 'ssl_dhparam /etc/nginx/ssl/dhparam.pem;' 'ssl_prefer_server_ciphers on;' 'ssl on;' 'listen 443 ssl;' '#SSL Part' ''
+printf "${PURPLE}Backup config /etc/nginx/sites-enabled/default${NC}\n"
+mkdir -p /etc/nginx/bakeup && cp -r /etc/nginx/sites-enabled/default /etc/nginx/bakeup/default.bak
+for each_line in 'ssl_ciphers "EECDH+AESGCM:EDH+AESGCM:AES256+EECDH:AES256+EDH";' 'ssl_protocols TLSv1 TLSv1.1 TLSv1.2;' 'ssl_certificate_key /etc/nginx/ssl/nginx.key;' 'ssl_certificate /etc/nginx/ssl/nginx.crt;' 'ssl_dhparam /etc/nginx/ssl/dhparam.pem;' 'ssl_prefer_server_ciphers on;' 'ssl on;' 'listen 443 ssl;' '#SSL Part' ''
 do	sed -i "/# Make site accessible from/ a \ \ \ \ \ \ \ \ ${each_line}" /etc/nginx/sites-enabled/default
 done
 service nginx reload && service nginx restart
