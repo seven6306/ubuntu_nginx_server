f875f7a1afdbbfe70fffbcf7726feb67f90eae20
seven6306 <31332405+seven6306@users.noreply.github.com>
2018-01-29 12:37:32 +0800

    add question

--- nginx_server.sh
+++ nginx_server.sh
@@ -4,14 +4,18 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 PURPLE='\033[1;35m'
 NC='\033[0m'
-
+Host=`ip a | awk '{print $2}' | grep -v '127.0.0.1' | grep -oE "([0-9]{1,3}\.){3}[0-9]{1,3}"`
 touch /etc/init.d/request.tmp >> /dev/null 2>&1
 [ $? -ne 0 ] && printf "${RED}ERROR: Permission denied, try \'sudo\' to execute the script.${NC}\n" && exit 1
 rm -f /etc/init.d/request.tmp
 [ `dpkg -l | awk '{print $2}' | grep -co nginx` -ne 0 ] && printf "${RED}ERROR: Service nginx is already installed.${NC}\n" && exit 1
 ping www.google.com -c 1 -q >> /dev/null 2>&1
 [ $? -ne 0 ] && printf "${RED}ERROR: Network is unavailable.${NC}\n" && exit 1
-printf "${PURPLE}Start installing nginx server...${NC}\n"
+read -p "Setup nginx server will take 30-60 minutes, Are you sure? [y/N]: " ans
+case $ans in
+	y*|Y*) printf "${PURPLE}Start installing nginx server...${NC}\n";;
+	*) exit 0
+esac
 printf "=============================================================================\n"
 apt-get update
 apt-get install nginx -y
@@ -22,10 +26,9 @@ printf "========================================================================
 openssl req -x509 -nodes -sha256 -days 365 -newkey rsa:2048 -keyout /etc/nginx/ssl/nginx.key -out /etc/nginx/ssl/nginx.crt -subj "/C=TW/ST=TAIPEI/L=Stockholm /O=A/OU=B/CN=C/emailAddress=xxx@xxx.com"
 openssl dhparam -out /etc/nginx/ssl/dhparam.pem 4096
 printf "=============================================================================\n\n"
-for each_line in 'ssl_ciphers "EECDH+AESGCM:EDH+AESGCM:AES256+EECDH:AES256+EDH";' 'ssl_protocols TLSv1 TLSv1.1 TLSv1.2;' 'ssl_certificate_key /etc/nginx/ssl/nginx.key;' 'ssl_certificate /etc/nginx/ssl/nginx.crt;' 'ssl_dhparam /etc/nginx/ssl/dhparam.pem;' 'ssl_prefer_server_ciphers on;' 'ssl on;' 'listen 443 ssl;' '#SSL Part' ''
-do
-	sed -i "/# Make site accessible from/ a \ \ \ \ \ \ \ \ ${each_line}" /etc/nginx/sites-enabled/default
+sed -i.bak '/# Make site accessible from/ a \ \ \ \ \ \ \ \ ssl_ciphers "EECDH+AESGCM:EDH+AESGCM:AES256+EECDH:AES256+EDH";' /etc/nginx/sites-enabled/default
+for each_line in 'ssl_protocols TLSv1 TLSv1.1 TLSv1.2;' 'ssl_certificate_key /etc/nginx/ssl/nginx.key;' 'ssl_certificate /etc/nginx/ssl/nginx.crt;' 'ssl_dhparam /etc/nginx/ssl/dhparam.pem;' 'ssl_prefer_server_ciphers on;' 'ssl on;' 'listen 443 ssl;' '#SSL Part' ''
+do	sed -i "/# Make site accessible from/ a \ \ \ \ \ \ \ \ ${each_line}" /etc/nginx/sites-enabled/default
 done
 service nginx reload && service nginx restart
-[ `service nginx status | grep -co not` -eq 0 ] && printf "${GREEN}Nginx server is running${NC} (Site: ${GREEN}https://`ip a | awk '{print $2}' | grep -v '127.0.0.1' | grep -oE "([0-9]{1,3}\.){3}[0-9]{1,3}"`${NC} Protocol: ${RED}Https 443 port${NC})\n" || printf "${RED}Sorry, nginx server is unavailable...${NC}\n"
-
+[ `service nginx status | grep -co not` -eq 0 ] && printf "\n${GREEN}Nginx server is running${NC} (Site: ${GREEN}https://${Host}${NC} Protocol: ${RED}Https 443 port${NC})\n\n" || printf "\n${RED}Sorry, nginx server is unavailable...${NC}\n\n"
