1486bf69728edb49a699e8cc827ed89d33934f45
seven6306 <31332405+seven6306@users.noreply.github.com>
2018-02-05 14:26:23 +0800

    Update nginx_server.sh

--- nginx_server.sh
+++ nginx_server.sh
@@ -7,7 +7,7 @@
 . lib/CheckPermission.sh
 . lib/declare_variables.sh
 
-CheckPermission && CheckInstall && NetworkConnTest www.google.com
+CheckPermission && CheckInstall --install && NetworkConnTest www.google.com
 Notification "Setup nginx server will take 30-60 minutes, Are you sure? [y/N]: " "${PURPLE}Start installing nginx server...${NC}\n${LINE}\n"
 [ `dpkg -l | awk '{print $2}' | grep -co nginx` -ne 0 ] && printf "${RED}ERROR: Server nginx is already installed.${NC}\n" && exit 1
 apt-get update
