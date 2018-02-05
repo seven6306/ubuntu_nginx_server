efb4de5db67407f9d14b8fe7a67b218b9587b6a4
seven6306 <31332405+seven6306@users.noreply.github.com>
2018-02-05 14:59:12 +0800

    Update CheckInstall.sh

--- lib/CheckInstall.sh
+++ lib/CheckInstall.sh
@@ -5,12 +5,12 @@ CheckInstall()
     if [ -d /etc/nginx -o -d /usr/local/nginx ]; then
         case $action in
             --install) printf "\033[0;31mERROR: Nginx server is already installed.\033[0m\n" && exit 1;;
-		    --remove)  printf "%s\t%31s\033[0;32m %s \033[0m]\n" " * Check if nginx is installed      " "[" "OK";;
+	    --remove)  printf "%s\t%31s\033[0;32m %s \033[0m]\n" " * Check if nginx is installed      " "[" "OK";;
         esac
     else
         case $action in
             --install) printf "%s\t%31s\033[0;32m %s \033[0m]\n" " * Check if nginx is installed      " "[" "OK" && return 0;;
-		    --remove)  printf "\033[0;31mERROR: Nginx server is not installed.\033[0m\n" && exit 1;;
+	    --remove)  printf "\033[0;31mERROR: Nginx server is not installed.\033[0m\n" && exit 1;;
         esac
     fi
 }
