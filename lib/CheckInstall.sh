0e5a0acaf06e2d70d465b3e19217cab90291e881
seven6306 <31332405+seven6306@users.noreply.github.com>
2018-02-06 12:51:42 +0800

    fixed summary

--- lib/CheckInstall.sh
+++ lib/CheckInstall.sh
@@ -5,16 +5,16 @@ CheckInstall()
     local action=$2
     local file=$3
     local dir=$4
-    local x=0
+    local int_x=0
     for f in `echo $file | sed 's,\,, ,g'`
-    do  [ -f $f ] && x=$(($x+1))
+    do  [ -f $f ] && local int_x=$(($int_x+1))
     done
     for d in `echo $dir | sed 's,\,, ,g'`
-    do  [ -d $d ] && x=$(($x+1))
+    do  [ -d $d ] && local int_x=$(($int_x+1))
     done
     case $action in
-        --install) [ $x -ne 0 ] && printf "\033[0;31mERROR: $pkgName is already installed.\033[0m\n" && exit 1;;
-        --remove)  [ $x -ne 0 ] && printf "\033[0;31mERROR: $pkgName is not installed.\033[0m\n" && exit 1;;
+        --install) [ $int_x -ne 0 ] && printf "\033[0;31mERROR: $pkgName is already installed.\033[0m\n" && exit 1;;
+        --remove)  [ $int_x -ne 0 ] && printf "\033[0;31mERROR: $pkgName is not installed.\033[0m\n" && exit 1;;
     esac
     printf "%s\t%31s\033[0;32m %s \033[0m]\n" " * Check if $pkgName is installed      " "[" "OK"
     return 0
