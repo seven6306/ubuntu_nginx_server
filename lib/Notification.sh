dbcf3e12ab165e72a51608a3e4b792909a118050
seven6306 <31332405+seven6306@users.noreply.github.com>
2018-02-06 16:33:56 +0800

    Update Notification.sh

--- lib/Notification.sh
+++ lib/Notification.sh
@@ -3,7 +3,7 @@ Notification()
 {
     read -p "$1" ans
     case $ans in
-        y*|Y*) printf "$2";;
-        *) exit 0;;
+        y*|Y*) printf "$2" && return 0;;
+        *) return 1;;
     esac
 }
