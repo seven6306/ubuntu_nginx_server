4920ee78af68b6fbfab887927666706bc3d6e214
seven6306 <31332405+seven6306@users.noreply.github.com>
2018-02-07 11:40:27 +0800

    Update CheckPermission.sh

--- lib/CheckPermission.sh
+++ lib/CheckPermission.sh
@@ -4,7 +4,7 @@ CheckPermission()
     local tmpfile=/etc/init.d/request.tmp
     touch $tmpfile >> /dev/null 2>&1
     [ $? -ne 0 ] && printf "\033[0;31mERROR: Permission denied, try \'sudo\' to execute the script.\033[0m\n" && exit 1
-    printf "%s\t%31s\033[0;32m %s \033[0m]\n" " * Check root permission in executed" "[" "OK"
+    printf "%s\t%34s\033[0;32m %s \033[0m]\n" " * Check root permission in executed" "[" "OK"
     rm -f $tmpfile
     return 0
 }
