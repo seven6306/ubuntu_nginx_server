bc969e4aa4002b2ea401985087a791f3a3d74f28
seven6306 <31332405+seven6306@users.noreply.github.com>
2018-02-07 11:43:33 +0800

    Update SSLGenerator.sh

--- lib/SSLGenerator.sh
+++ lib/SSLGenerator.sh
@@ -6,10 +6,10 @@ SSLGenerator()
     printf "\n\033[1;35mGenerate SSL Certification:\033[0m\n${LINE}\n"
     openssl req -x509 -nodes -sha256 -days 365 -newkey rsa:2048 -keyout ${nginx_dir}/ssl/nginx.key -out ${nginx_dir}/ssl/nginx.crt -subj "/C=TW/ST=TAIPEI/L=Stockholm /O=A/OU=B/CN=C/emailAddress=xxx@xxx.com"
     if [ -f ${nginx_dir}/nginx.key -a -f ${nginx_dir}/ssl/nginx.crt ]; then
-        printf "${LINE}\n\n\033[0;32m * \033[0m%s\t%24s\033[0;32m %s \033[0m]\n" "Generate SSL certification encrypt: sha256" "[" "OK"
+        printf "${LINE}\n\n\033[0;32m * \033[0m%s\t%26s\033[0;32m %s \033[0m]\n" "Generate SSL certification encrypt: sha256" "[" "OK"
         return 0
     else
-        printf "${LINE}\n\n\033[0;31m * \033[0m%s\t%24s\033[0;31m%s\033[0m]\n" "Generate SSL certification encrypt: sha256" "[" "Fail"
+        printf "${LINE}\n\n\033[0;31m * \033[0m%s\t%26s\033[0;31m%s\033[0m]\n" "Generate SSL certification encrypt: sha256" "[" "Fail"
         return 1
     fi
 }
