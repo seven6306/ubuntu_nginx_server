5dc21651436ef2ffaea0294157c582dfb07401fb
seven6306 <31332405+seven6306@users.noreply.github.com>
2018-02-13 10:06:31 +0800

    Update SSLgenerator.py

--- lib/SSLgenerator.py
+++ lib/SSLgenerator.py
@@ -13,8 +13,8 @@ if __name__ == '__main__':
         print '\033[1;35mGenerate SSL Certification:\033[0m'
         create_self_signed_cert(join(ssl_dir, cert_name))
         if not isfile(join(ssl_dir, cert_name + '.key')) or not isfile(join(ssl_dir, cert_name + '.crt')):
-            print "\033[0;31m * \033[0m%s\t%26s\033[0;31m%s\033[0m]" % ("Generate SSL certification encrypt: sha256", "[", "Fail")
+            print "\033[0;31m * \033[0m%s\t%26s\033[0;31m%s\033[0m]\n" % ("Generate SSL certification encrypt: sha256", "[", "Fail")
             raise SystemExit(3)
-        print "\033[0;32m * \033[0m%s\t%26s\033[0;32m %s \033[0m]" % ("Generate SSL certification encrypt: sha256", "[", "OK")
+        print "\033[0;32m * \033[0m%s\t%26s\033[0;32m %s \033[0m]\n" % ("Generate SSL certification encrypt: sha256", "[", "OK")
     else:
         print 'Usage: python SSLgenerator.py [PATH]'
